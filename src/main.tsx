6129e2f027ca03f83d766c652350af7746036b75
jayesh suthar <39787261+jayesh1598@users.noreply.github.com>
2025-12-13 18:49:58 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import AdminGuard from './components/AdminGuard';
+import AdminDashboard from './pages/admin/Dashboard';
+import AdminLogin from './pages/admin/Login';
+createRoot(document.getElementById("root")!).render(<App />);
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+<Routes>
+  <Route path="/admin/login" element={<AdminLogin />} />
+
+  <Route
+    path="/admin/dashboard"
+    element={
+      <AdminGuard>
+        <AdminDashboard />
+      </AdminGuard>
+    }
+  />
+</Routes>;
